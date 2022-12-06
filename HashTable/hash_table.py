6ac118aff59d24fd91b039c2aa29a2e93d64d176
asclaudino <arielsclaudino@gmail.com>
2022-12-06 16:05:20 -0300

    HashTable implemented

--- HashTable/hash_table.py
+++ HashTable/hash_table.py
@@ -9,28 +9,39 @@ def txt2hashtable(path):
         line = dict_file.readline()
         if not line:
             break
-        dict_data.update({line.strip():index})
-        dict_list.append(line.strip())
+        dict_data.update({line.strip().lower():index})
+        dict_list.append(line.strip().lower())
         index += 1
     dict_file.close()
     return dict_data,dict_list
 
-def dict_constains_word(dictionary, dictio2list, word):
-        if word in  dictionary:
-            index = dictionary.get(word)
+def dict_constains_word(dictionary, dictio2list, word,n):
+        word_lower = word.lower()
+        if  word_lower in  dictionary:
+            index = dictionary.get( word_lower)
             answer = []
+            length = len(word_lower)
+            answer.append( word_lower)
             
-            answer.append(word)
-            
-        # tenho que cortar as proximas palavras da lista e comparar com a palavra dada, se bater, printa a próxima em questão.
-            iterator = index + 1
-            if iterator <= len(dictio2list)-1:
-                answer.append( dictio2list[index+1])
+        # comparing the suffixes of the next words in the file with the given word.
+            for i in range(n-1): 
+                index = index + 1  
+                if  word_lower[:length] == dictio2list[index][:length] and index <= len(dictio2list)-1:
+                    answer.append( dictio2list[index])
+                else:
+                    break
             return answer
         else:
             return []
 
-(dictionary,list) = txt2hashtable('dictionary.txt')
 
-word = 'jan'
-print(dict_constains_word(dictionary,list,word))
+## How to use the functions
+
+##Constructing the HashTable and the List
+#(dictionary,list) = txt2hashtable('dictionary.txt')
+
+##Searched word
+#word = 'jan'
+
+## Getting the results
+#print(dict_constains_word(dictionary,list,word,6))

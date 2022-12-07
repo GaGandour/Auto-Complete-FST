017341b8799d35d979cd60a7b8cc5c8cf139aadd
Gabriel Gandour <gagandour@gmail.com>
2022-12-07 20:55:04 -0300

    a

---
+++ Compare/compare_word_search.py
@@ -0,0 +1,26 @@
+import time
+from AutoComplete.autocomplete import first_n_correspondent_words
+
+
+def calculate_fst_search_time(fst, word):
+    start_time = time.time()
+    first_n_correspondent_words(fst, word)
+    return time.time() - start_time
+
+
+
+
+
+
+
+def generate_test_words():
+    return [
+        "Monday",
+        "Ariel",
+        "Emanuel",
+        
+    ]
+
+
+
+

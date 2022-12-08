c65d8c1e3c90b282f532acc6c00a5d1b73be8e28
asclaudino <arielsclaudino@gmail.com>
2022-12-07 21:53:40 -0300

    generating the tests words vectors

--- Compare/compare_word_search.py
+++ Compare/compare_word_search.py
@@ -1,4 +1,5 @@
 import time
+import random
 from AutoComplete.autocomplete import first_n_correspondent_words
 from Levenshtein.search_1distance import search1DistanceWord
 
@@ -14,17 +15,34 @@ def calculate_fst_search_time(fst, word):
 
 
 
-def generate_test_words():
-    return [
-        "Monday",
-        "Ariel",
-        "Emanuel",
+def generate_test_words(fst_file, number_of_words):
+    file = open(fst_file, encoding='latin-1')
+    lines_indexes = []
+    
+    for i in range(number_of_words):
+        lines_indexes.append(random.randomint(0,100000))
+    
+    lines_indexes.sort()
+    lines_data = []
+    
+    for i, line in enumerate(file):
+        if i in lines_indexes:
+            lines_data.append(line.strip())
+        elif i > lines_indexes[-1]:
+            break
+    
+    return lines_data
+    
+    
+def generate_test_words_splitted(test_words):
+    splitted_words = []
+    
+    for word in test_words: 
+        split_index = random.randint(1,len(word))
+        splitted_words.append(word[:split_index])
         
-    ]
-
-
-
-
+    return splitted_words
+    
 def calculate_levenshtein_time(fst, word):
 
     start_time = time.time()

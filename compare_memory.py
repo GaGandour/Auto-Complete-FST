508c41c93714a57902ab78de7fd4900d31cacc52
Gabriel Gandour <gagandour@gmail.com>
2022-12-07 23:24:14 -0300

    memory usage

--- compare_memory.py
+++ compare_memory.py
@@ -1,5 +1,7 @@
-from Compare.compare_search_memory import get_FST_memory
+from Compare.compare_search_memory import get_FST_memory, get_hash_table_memory, get_levenshtein_memory
 
 
 if __name__ == "__main__":
     get_FST_memory()
+    get_hash_table_memory()
+    get_levenshtein_memory()
